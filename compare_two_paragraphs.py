93c8cfffb72574fa08231d0340b2caa8b7e7d76f
JohnFunkCode <email@johnfunk.com>
2018-01-28 11:19:36 -0700

    Added lessons for Jan 28

--- compare_two_paragraphs.py
+++ compare_two_paragraphs.py
@@ -1,26 +1,17 @@
-# Compare Two Paragraphs
+
+
+
 from src import textanalytics as ta
+# userwords1=input("enter some words:")
+# userwords2=input("enter some words:")
+# d1=ta.wordcount(userwords1)
+# d2=ta.wordcount(userwords2)
+# print("Words from the first sentence are: ",d1)
+# print("Words from the second sentence are: ",d2)
 
-#get input from user
-p1=input("Enter paragraph 1: ")
-p2=input("Enter paragraph 2: ")
-
-#get the wordcount dictionaries from the textanalytics pacakge
-d1=ta.wordcount(p1)
-d2=ta.wordcount(p2)
-
-# Show the union - words in paragraph 1 that are in paragraph 2
-l=ta.a_union_b(d1,d2)
-print("Words in both paragraphs")
-print(l)
-
-# Show words in Paragraph 1 that aren't in Paragraph 2
-l=ta.in_a_not_in_b(d1,d2)
-print("Words in Paragraph 1 that aren't in Paragraph 2")
-print(l)
-
-
-# Show words in Paragraph 2 that aren't in Paragraph 1
-l=ta.in_a_not_in_b(d2,d1)
-print("Words in Paragraph 2 that aren't in Paragraph 1")
-print(l)
+userwords1=input("enter some words:")
+userwords2=input("enter some words:")
+u=ta.union(userwords1, userwords2)
+print("THe common words are:")
+for word in u:
+    print(word)
--- src/textanalytics.py
+++ src/textanalytics.py
@@ -1,4 +1,3 @@
-
 def wordcount(userwords):
 
     wordlist=userwords.split()
@@ -9,56 +8,52 @@ def wordcount(userwords):
             wordcount[word] += 1
         else:
             wordcount[word] = 1
-    return wordcount
+    return(wordcount)
 
-def a_union_b(a,b):
-    union = list()
-    keysa=a.keys()
-    keysb=b.keys()
-    for word in keysa:
-        if word in keysb:
-#            print("found :" + word)
-            union.append(word)
-    return union
+    #for key,value in wordcount.items():
+      #print(key,"\t",value)
+
+def union(paragraph1, paragraph2):
+    common_words = list()
+    wordcount1 = wordcount(paragraph1)
+    wordcount2 = wordcount(paragraph2)
+
+    keys1 = wordcount1.keys()
+    keys2 = wordcount2.keys()
+
+    for word in keys1:
+        if word in keys2:
+            #print("Supermatch! {} was found!".format(word))
+            common_words.append(word)
+        #else:
+            #print("Sorry! {} was not found.".format(word))
+    return common_words
+
+def test_union():
+    s1 = ("one two")
+    s2 = ("two three four")
+    common_words = union(s1, s2)
 
 
-def in_a_not_in_b(a, b):
-    a_not_b = list()
-    keysa=a.keys()
-    keysb=b.keys()
-    for word in keysa:
-        if word not in keysb:
-#            print("count not find :" + word)
-            a_not_b.append(word)
-#        else:
-#            print("found :" + word)
-    return a_not_b
+    assert(len(common_words) == 1)
 
-def main():
-    #One Word Tests
-    d1=wordcount("one")
-    assert len(d1)==1   # test there is only 1 word in the result
-    assert d1["one"]==1 # test count of the word "one" is 1
+    assert(common_words[0] == 'two')
 
-    #Three Word Tests
-    d2=wordcount("one two three")
-    assert len(d2)==3  # test there are 3 words in the result
-    assert d2["one"]==1 # test each word has a count of 1
-    assert d2["two"]==1
-    assert d2["three"]==1
 
-    #test that a_union_b(['one','two','three'],['one']) returns 'one'
-    u=ta.a_union_b(d1,d2)
-    assert(u==['one'])
 
-    #test that in_a_not_in_b of "one", "one" return an empty set
-    assert in_a_not_in_b(d1,d1)==[]
+def test_union2():
+    s2 = ("one two")
+    s1 = ("two three four")
+    common_words = union(s1, s2)
 
-    #test that in_a_not_in_b of ['one','two','three'],['one'] returns a set with both 'two' and 'three' in it
-    l=in_a_not_in_b(d2,d1)
-    assert("two" in l)
-    assert("three" in l)
+
+    assert(len(common_words) == 1)
+
+    assert(common_words[0] == 'two')
 
 
 if __name__ == "__main__":
-    main()
+    print("This is test analytics")
+    test_union()
+    test_union2()
+    print('All good, bro!')
